e96711c8a46c8775b6a05d0afc96e627a61dafa1
PiedraL <joaquinlangone12@gmail.com>
2024-02-25 18:56:14 -0300

    step 2. Grid and iu

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MoviesContainer } from './components/MoviesContainer'
+
+import resoultsMovies from "./mocks/with-results.json"
+// import resoultsMoviesFail from "./mocks/no-results.json"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const movies = resoultsMovies.Search
+  // const hasMovies = movies?.length > 0
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <header>
+        <h1>Movie Searcher</h1>
+        <form className='form'>
+          <input type='search' placeholder='Search your movie'></input>
+          <button type='submite'>Search</button>
+        </form>
+      </header>
+
+      <main>
+
+        <MoviesContainer movies={movies}/>
+
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/MoviesContainer.jsx
@@ -0,0 +1,66 @@
+import '../App.css'
+// import resultsMovies from '../mocks/with-results.json'
+
+function ListOfMovies ({ movies }) {
+    return(
+        <div className='movie-container'>
+            {
+                movies.map(movie => (
+                    <div key={movie.imdbID}  className='movie-box'>
+                        <h2>{movie?.Title?.length > 14 ? movie.Title.slice(0, 14) + "..." : movie.Title}</h2>
+                        <img src={movie.Poster} alt={movie.Title} />
+                        <p>{movie.Year}</p>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+function NoMoviesResult () {
+    return(
+        <div>
+            <p>Movies do not found</p>
+        </div>
+    )
+}
+
+export function MoviesContainer({ movies }) {
+
+    const hasMovies = movies?.length > 0
+
+    return(
+        
+            hasMovies 
+            ? <ListOfMovies movies={movies}/>
+            : <NoMoviesResult/> 
+        
+    )
+
+} 
+
+// export const MoviesContainer = ({ movies }) => {
+
+//     // const movies = resultsMovies.Search
+//     const hasMovies = movies?.length > 0
+
+//     console.log(movies);
+
+//     return (
+//         <div className='movie-container'>
+//             {
+//                 hasMovies ? 
+//                     movies.map(movie => (
+//                       <div key={movie.imdbID}  className='movie-box'>
+//                           <h2>{movie?.Title?.length > 14 ? movie.Title.slice(0, 14) + "..." : movie.Title}</h2>
+//                           <img src={movie.Poster} alt={movie.Title} />
+//                           <p>{movie.Year}</p>
+//                       </div>
+//                     ))
+//                 :
+//                 <span>No movies :(</span> 
+//             }
+//         </div>
+//     )
+
+// }
