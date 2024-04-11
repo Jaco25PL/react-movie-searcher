93c10dcc3c6317c970374ce4510a5d1107622d15
PiedraL <joaquinlangone12@gmail.com>
2024-04-11 01:45:57 -0300

    Test done in a basic way

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # React + Vite
 
+# TECHNICAL TEST
+
+**Requirements**
+
+-  Show an input field to search movies and a search button (form)
+- Lists the found movies and show its title, year, and poster (grid display)
+- Grid responsive for the movies lists
+
+**First Iteration**
+
+- Avoid the same search twice
+- Do the search automatically when the user is typing
+- Avoid the search to be continuously while typing (debounce)
+
+
+
+
+
+
+
+
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,113 @@
-import './App.css'
 import { useState } from 'react'
-
-// import resultsMovies from './mocks/with-results.json'
-import { MoviesContainer } from './components/MoviesContainer'
-
+import './App.css'
+import { useEffect } from 'react'
+// import moviesResults from './mocks/withResults.json'
 
 function App() {
 
-  const [userSearch, setUserSearch] = useState("")
-  // const movies = resultsMovies.Search
+  // const movies = moviesResults.Search
 
-  // const mapMovies = () => {
-  //   movies.map(movie => (
-  //     <div key={movie.imdbID}>
-  //       <img src={movie.Poster} alt={movie.imdbID} />
-  //     </div>
-  //   ))
-  // }
+  const [ search, setSearch ] = useState('deadpool')
+  const [ movies, setMovies ] = useState()
+  const [ onSearch, setOnSearch ] = useState()
 
+  useEffect( () => {
+    
+    const URL = `https://www.omdbapi.com/?apikey=4287ad07&s=${search}`
+    // fetch(URL)
+    //   .then(res => res.json())
+    //   .then(data => setMovies(data.Search))
+    
+    const tryingToFetch = async () => {
+      
+      try {
+        const response = await fetch(URL)
+				if(!response.ok){
+					throw new Error(`Failed to fetch data. Status: ${response.status}`)
+				}
+				const docs = await response.json()
+
+        const mappedMovies = docs.Search?.map(movie => ({
+          id: movie.imdbID,
+          image: movie.Poster,
+          title: movie.Title,
+          year: movie.Year,
+          type: movie.type
+        }))
+
+        setMovies(mappedMovies)
+
+      } catch (error) {
+        console.log("Error fetchaing data", error)
+				throw error
+      }
+    }
+
+    tryingToFetch()
+
+  },[search])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    
-    const {query} = Object.fromEntries( new window.FormData(e.target) )
-    setUserSearch(query)
+
+    if(onSearch === '' || !onSearch){
+      console.log("false")
+    }else{
+      setSearch(onSearch)
+    }
   }
 
-  console.log(userSearch)
-  
+  const handleChange = (e) => {
+
+    const query = e.target.value
+    setOnSearch(query)
+    // setSearch(query)
+
+  }
+
+
   return (
-    <div className='w-full'>
-      <header className='mt-10'>
-        <div>
-          <form onSubmit={handleSubmit} className='flex gap-5 bg-slate-900 py-3 px-2 rounded-lg'>
-            <input 
-              className='bg-slate-900 outline-none text-slate-50 px-2 py-1 text-xl'
-              name='query' 
-              type="search"
-              // ref={inputRef} 
-              placeholder='Type a movie'
-            />
-            <button 
-              type='submit'
-              className='hover:text-slate-400 transition-all duration-200 active:text-slate-500 active:scale-95 ring-2 ring-slate-700  text-md mr-2 bg-gradient-to-tr from-slate-900 to-slate-800 px-2 rounded-md  text-slate-50 font-bold'
-            >Search</button>
-          </form>
-        </div>
+
+    <div className='w-full '>
+
+      <header>
+        <h1 className='font-bold text-5xl text-gray-50 w-fit mx-auto mt-10'>MOVIE SEARCHER</h1>
       </header>
 
-      <main className='mx-10'>
-        <MoviesContainer/>
-      </main>  
+      <nav className='w-fit my-10 mx-auto'>
+        <form onSubmit={handleSubmit} className='flex gap-3'>
+
+          <input onChange={handleChange} className='bg-violet-900 text-gray-50 font-semibold p-3 rounded-lg focus:outline-none focus:bg-violet-700 transition-colors duration-300 ease-in-out' type="text" name='query' title='search' placeholder='Search a movie' />
+          <button className='bg-violet-900 text-gray-50 font-semibold p-3 rounded-lg hover:bg-violet-700 transition-colors duration-300 ease-in-out'  type='submit'>Search</button>
+
+        </form>
+      </nav>
+
+      <main className='w-full p-10'>
+        <div className='justify-items-center gap-10 grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))]'>
+
+          {
+            movies?.length > 0
+              ? (
+                movies.map(movie => (
+                  <div key={movie.id} className='flex flex-col items-center'>
+
+                    <img className='rounded-lg' src={movie.image} alt={movie.title} />
+                    <h3 className='my-2 text-2xl font-semibold text-gray-50'>{movie.title}</h3>
+                    <p className='text-lg font-medium text-gray-50'>{movie.year}</p>
+                  </div>
+                ))
+              ) : (
+                <span className='font-bold text-gray-50'>Movies not found</span>
+              )
+          }
+          
+        </div>
+      </main>
 
     </div>
   )
+
 }
 
 export default App
