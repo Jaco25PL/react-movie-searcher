e6eaae925ffef1d355d07fa7bdaf23bcac317c38
PiedraL <joaquinlangone12@gmail.com>
2024-05-07 00:33:08 -0300

    Add reference comments in useSearch hook

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -5,16 +5,16 @@ export function useSearch () {
     const [ search, setSearch ] = useState('') // what we have from the input
     const [ error, setError ] = useState(null)
     const firstInput = useRef(true)
-  
+
     useEffect(() => {
-  
+      
       if(firstInput.current){
-        firstInput.current = search === ''
-        setError(true)
+        firstInput.current = search === '' // if search empty we set ref as true, once is not empty we set the ref as false, but as in the if we said that should only enter id the ref is true, never will enter again
+        setError(true) // Becuase in the app we render things only if error is false
         return
       }
       if(search === ''){
-        setError("Cant't search nothing")
+        setError("Can't search nothing") 
         return
       }
       if(search.length < 3){
